1c89e21a4b2b4e54519639dbf6b8001fc83518a3
mamasza <107420170+mamasza@users.noreply.github.com>
2022-07-14 20:11:40 +0200

    20220714

---
+++ script.js
@@ -0,0 +1,38 @@
+const data =[
+    { title:"Różowy",
+      src: "1.webp",
+    },
+    { title:"niebieski",
+      src: "2.webp",
+    },
+    { title:"chmurki",
+      src: "3.webp",
+    }
+]
+function changeHTMLInfo(index){
+    
+    const title = data[index].title;
+    const titleDOM = document.querySelector("h2")
+    titleDOM.innerHTML = title
+    
+    const image = data[index].src
+    const imageDOM = document.querySelector("img")
+    imageDOM.src = "/img/"+image
+    
+    
+
+    const lewaDOM = document.querySelector('.leftArr');
+    lewaDOM.onclick =function(){
+        index--;
+        if (index < 0) index = 2
+        changeHTMLInfo(index);
+       
+    }
+    const prawaDOM = document.querySelector(".rightArr");
+    prawaDOM.onclick = function(){
+        index++;
+        if (index>2) {index = 0}
+        changeHTMLInfo(index);
+    }
+}
+changeHTMLInfo(1)
